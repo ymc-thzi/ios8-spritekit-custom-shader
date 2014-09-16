1fbadcff9a144bb4f08d5810da18092fb5080bab
Thomas Zinnbauer <ymc-thzi@vcs.ymc.ch>
2014-09-16 17:25:29 +0200

    added movement shader

--- ios8-spritekit-custom-shader/GameScene.m
+++ ios8-spritekit-custom-shader/GameScene.m
@@ -12,31 +12,41 @@
 
 -(void)didMoveToView:(SKView *)view {
     
-    //Setup a background
-    SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:@"sand.png"];
-    background.size = self.size;
-    background.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
-    [self addChild:background];
-    
-    //Setup a container sprite for the shader
-    SKSpriteNode *shaderContainer = [SKSpriteNode spriteNodeWithImageNamed:@"dummypixel.png"];
-    shaderContainer.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
-    shaderContainer.size = CGSizeMake(self.frame.size.width, self.frame.size.height);
-    [self addChild:shaderContainer];
+    //Setup a container sprite for the shader that makes the movement
+    SKSpriteNode *shaderContainer_move = [SKSpriteNode spriteNodeWithImageNamed:@"dummypixel.png"];
+    shaderContainer_move.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+    shaderContainer_move.size = CGSizeMake(self.frame.size.width, self.frame.size.height);
+    [self addChild:shaderContainer_move];
     
     //Create the shader from a shader-file
-    SKShader* shader = [SKShader shaderWithFileNamed:@"shader_water.fsh"];
+    SKShader* shader_move = [SKShader shaderWithFileNamed:@"shader_water_movement.fsh"];
     //Set vairiables that are used in the shader script
-    shader.uniforms = @[
-                        [SKUniform uniformWithName:@"size" floatVector3:GLKVector3Make(self.frame.size.width, self.frame.size.height, 0)],
+    shader_move.uniforms = @[
+                        [SKUniform uniformWithName:@"size" floatVector3:GLKVector3Make(self.frame.size.width*1.2, self.frame.size.height*1.5, 0)],
+                        [SKUniform uniformWithName:@"customTexture" texture:[SKTexture textureWithImageNamed:@"sand.png"]],
                         ];
     //add the shader to the sprite
-    shaderContainer.shader = shader;
+    shaderContainer_move.shader = shader_move;
     
-    //just add one more sprite only to look good :-) 
+    //Setup a container sprite for the shader that makes the reflections
+    SKSpriteNode *shaderContainer_reflect = [SKSpriteNode spriteNodeWithImageNamed:@"dummypixel.png"];
+    shaderContainer_reflect.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+    shaderContainer_reflect.size = CGSizeMake(self.frame.size.width, self.frame.size.height);
+    [self addChild:shaderContainer_reflect];
+    
+    //Create the shader from a shader-file
+    SKShader* shader_reflect = [SKShader shaderWithFileNamed:@"shader_water_reflection.fsh"];
+    //Set vairiables that are used in the shader script
+    shader_reflect.uniforms = @[
+                             [SKUniform uniformWithName:@"size" floatVector3:GLKVector3Make(self.frame.size.width, self.frame.size.height, 0)],
+                          ];
+    //add the shader to the sprite
+    shaderContainer_reflect.shader = shader_reflect;
+    
+    //just add sand sprite only to look good :-)
     SKSpriteNode *beach = [SKSpriteNode spriteNodeWithImageNamed:@"beach.png"];
     beach.size = self.size;
-    beach.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+    beach.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2-100);
     [self addChild:beach];
 }
 
