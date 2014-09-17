4048c9c6c5d2a3ed20e32d858a62f65e6f7406f0
Thomas Zinnbauer <ymc-thzi@vcs.ymc.ch>
2014-09-17 11:46:41 +0200

    secondshader and some shells and seastars

--- ios8-spritekit-custom-shader/GameScene.m
+++ ios8-spritekit-custom-shader/GameScene.m
@@ -17,23 +17,22 @@
     shaderContainer_move.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
     shaderContainer_move.size = CGSizeMake(self.frame.size.width, self.frame.size.height);
     [self addChild:shaderContainer_move];
-    
     //Create the shader from a shader-file
     SKShader* shader_move = [SKShader shaderWithFileNamed:@"shader_water_movement.fsh"];
     //Set vairiables that are used in the shader script
     shader_move.uniforms = @[
-                        [SKUniform uniformWithName:@"size" floatVector3:GLKVector3Make(self.frame.size.width*1.2, self.frame.size.height*1.5, 0)],
+                        [SKUniform uniformWithName:@"size" floatVector3:GLKVector3Make(self.frame.size.width*1.5, self.frame.size.height*1.5, 0)],
                         [SKUniform uniformWithName:@"customTexture" texture:[SKTexture textureWithImageNamed:@"sand.png"]],
                         ];
     //add the shader to the sprite
     shaderContainer_move.shader = shader_move;
+    
     
     //Setup a container sprite for the shader that makes the reflections
     SKSpriteNode *shaderContainer_reflect = [SKSpriteNode spriteNodeWithImageNamed:@"dummypixel.png"];
     shaderContainer_reflect.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
     shaderContainer_reflect.size = CGSizeMake(self.frame.size.width, self.frame.size.height);
     [self addChild:shaderContainer_reflect];
-    
     //Create the shader from a shader-file
     SKShader* shader_reflect = [SKShader shaderWithFileNamed:@"shader_water_reflection.fsh"];
     //Set vairiables that are used in the shader script
@@ -42,6 +41,7 @@
                           ];
     //add the shader to the sprite
     shaderContainer_reflect.shader = shader_reflect;
+    
     
     //just add sand sprite only to look good :-)
     SKSpriteNode *beach = [SKSpriteNode spriteNodeWithImageNamed:@"beach.png"];
